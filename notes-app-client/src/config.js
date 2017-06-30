4d592d9859dc135c720db2cb3efe5d75862a5456
nick.ramsbottom <nick.ramsbottom@hogarthww.com>
2017-06-30 15:14:20 +0100

    file upload added to newnote page

--- notes-app-client/src/config.js
+++ notes-app-client/src/config.js
@@ -1,10 +1,15 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    BUCKET: 'hogarthww-notes-app-uploads',
+  },
   apiGateway: {
     URL: 'https://dd6e1gnjze.execute-api.eu-west-1.amazonaws.com/prod',
   },
   cognito: {
     USER_POOL_ID: 'eu-west-1_cKz8C6FNb',
     APP_CLIENT_ID: '5phi59qeo71alcnuhdoa81ftsp',
+    REGION: 'eu-west-1',
+    IDENTITY_POOL_ID: 'eu-west-1:378453a5-1ed7-492f-9647-fee8c1fc8c87',
   },
 };
--- notes-app-client/src/libs/awsLib.js
+++ notes-app-client/src/libs/awsLib.js
@@ -1,3 +1,5 @@
+import AWS from 'aws-sdk';
+
 import config from '../config';
 
 export async function invokeApig(
@@ -22,4 +24,37 @@ export async function invokeApig(
   }
 
   return results.json();
+}
+
+export function getAwsCredentials(userToken) {
+  const authenticator = `cognito-idp.${config.cognito.REGION}.amazonaws.com/${config.cognito.USER_POOL_ID}`;
+
+  AWS.config.update({ region: config.cognito.REGION });
+
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: config.cognito.IDENTITY_POOL_ID,
+    Logins: {
+      [authenticator]: userToken,
+    },
+  });
+
+  return AWS.config.credentials.getPromise();
+}
+
+export async function s3Upload(file, userToken) {
+  await getAwsCredentials(userToken);
+
+  const s3 = new AWS.S3({
+    params: {
+      Bucket: config.s3.BUCKET,
+    }
+  });
+  const filename = `${AWS.config.credentials.identityId}-${Date.now()}-${file.name}`;
+
+  return s3.upload({
+    Key: filename,
+    Body: file,
+    ContentType: file.type,
+    ACL: 'public-read',
+  }).promise();
 }
